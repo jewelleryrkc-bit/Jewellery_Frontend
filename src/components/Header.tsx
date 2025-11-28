e566d1fa35f7d32117753d007d270e4d5db158eb
= <jewelleryrkc@gmail.com>
2025-11-28 17:28:19 +0530

    navbar and scroling effect fix as amazon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,31 +15,35 @@ export default function Header({ user }: HeaderProps) {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
-    let lastScrollPos = window.scrollY;
-    let ticking = false;
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-      if(!ticking){
-        window.requestAnimationFrame(() => {
-          setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
-          lastScrollPos = currentScrollPos;
-          ticking = false;
-        });
-        ticking = true;
-        return;
-      }
-    };
+  let lastScrollY = window.scrollY;
+  const THRESHOLD = 25;
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos, visible]);
+  const handleScroll = () => {
+    const current = window.scrollY;
+
+    if (current < 0.5) {
+      setVisible(true);
+    } else if (current > lastScrollY) {
+      setVisible(false); // scrolling down
+    } else if (lastScrollY - current > THRESHOLD) {
+      setVisible(true); // strong scroll up
+    }
+
+    lastScrollY = current;
+  };
+
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+}, []);
 
   return (
     // <header className="w-full top-0 left-0 right-0 bg-white shadow-md">
-    <header className={`fixed top-0 left-0 right-0 w-full bg-white shadow-md z-[9999] transition-transform duration-300 ${
-    visible ? "translate-y-0" : "-translate-y-full"
-  }`}
-  style={{ willChange: "transform" }}
+    <header  className={`fixed top-0 left-0 right-0 w-full bg-white shadow-md z-[9999]
+      transition-transform duration-300 ${
+        visible ? "translate-y-0" : "-translate-y-full"
+      }`}
+      style={{ willChange: "transform" }}
+    
   >
       <TopHeader />
       <MiddleHeader />
